dace44ad2836ddf0085c6e4018841c5fa87bfb37
Stefan <Stefan@Laptop-Stefan>
2013-06-14 08:58:28 +0200

    hopefully removed closing exception

--- WWE/src/wwe/Activator.java
+++ WWE/src/wwe/Activator.java
@@ -34,8 +34,8 @@ public class Activator extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
 		super.stop(context);
+		plugin = null;
 	}
 
 	/**
